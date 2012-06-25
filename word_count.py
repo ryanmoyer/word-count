06ccb8696ae711ce7967eb7c327d36dae0be2cac
Ryan Moyer <rymoyer@gmail.com>
2012-06-24 21:07:59 -0400

    Prints every word in a column.

--- word_count.py
+++ word_count.py
@@ -4,11 +4,7 @@
 print 'Word Count'
 print 'Tells how many words are in a document.'
 input_file_name = raw_input('Please enter the name of the document you would like to words counted: ')
-# The with statement below does the same as the following code:
-# input_file = open(input_file_name)
-# print input_file.readline()
-# input_file.close()
 with open(input_file_name) as input_file:
-    #print input_file.readline()
     for line in input_file:
-        print line,
+        for word in line.split():
+            print word
