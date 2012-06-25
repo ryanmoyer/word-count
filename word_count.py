878de8445e22c9d867dc3690292048fde3e72fba
Ryan Moyer <rymoyer@gmail.com>
2012-06-24 21:45:45 -0400

    Counts the number of each individual word with bad output formatting.

--- word_count.py
+++ word_count.py
@@ -4,7 +4,13 @@
 print 'Word Count'
 print 'Tells how many words are in a document.'
 input_file_name = raw_input('Please enter the name of the document you would like to words counted: ')
+word_count = {}
 with open(input_file_name) as input_file:
     for line in input_file:
         for word in line.split():
-            print word
+            if word in word_count:
+                word_count[word] = word_count[word] + 1
+            else:
+                word_count[word] = 1
+print word_count
+
