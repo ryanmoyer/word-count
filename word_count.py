408117094a69aba69d96cc7bc6b92a490ff79680
Ryan Moyer <rymoyer@gmail.com>
2012-08-09 20:59:01 -0400

    Changed into a function.

--- word_count.py
+++ word_count.py
@@ -1,16 +1,10 @@
-# Word Count
-# Author: Ryan Moyer
-
-print 'Word Count'
-print 'Tells how many words are in a document.'
-input_file_name = raw_input('Please enter the name of the document you would like to words counted: ')
-word_count = {}
-with open(input_file_name) as input_file:
-    for line in input_file:
-        for word in line.split():
-            if word in word_count:
-                word_count[word] = word_count[word] + 1
-            else:
-                word_count[word] = 1
-for word, count in word_count.iteritems():
-    print '{0}: {1}'.format(word, count)
+def count_words_in_file(file_name):
+    word_count = {}
+    with open(file_name) as input_file:
+        for line in input_file:
+            for word in line.split():
+                if word in word_count:
+                    word_count[word] = word_count[word] + 1
+                else:
+                    word_count[word] = 1
+    return word_count
