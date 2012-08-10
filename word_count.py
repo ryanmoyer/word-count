4add5057876053e426787d06a9f9c1f1c51a6ab6
Ryan Moyer <rymoyer@gmail.com>
2012-08-09 22:06:41 -0400

    Changed dictionary to defaultdict.

--- word_count.py
+++ word_count.py
@@ -1,10 +1,9 @@
+from collections import defaultdict
+
 def count_words_in_file(file_name):
-    word_count = {}
+    word_count = defaultdict(int)
     with open(file_name) as input_file:
         for line in input_file:
             for word in line.split():
-                if word in word_count:
-                    word_count[word] = word_count[word] + 1
-                else:
-                    word_count[word] = 1
+                word_count[word] += 1
     return word_count
